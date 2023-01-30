2d6d3f16023928351693b05edd2c01573e9aa880
linyuxuanlin <824676271@qq.com>
2023-01-30 17:08:11 +0800

    add folder-to-html

---
+++ example.js
@@ -0,0 +1,15 @@
+let generator = require('./index');
+
+generator({
+  exclude: [
+  /node_modules/,
+  /\.git/,
+  /style/,
+  /template/,
+  /example.js/,
+  /index.html/,
+  /index.js/,
+  /package-lock.json/,
+  /package.json/,
+  ]
+});
